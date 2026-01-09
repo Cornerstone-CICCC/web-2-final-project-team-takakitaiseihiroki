1fb1544cafe7bc3bd6699d774c64ae4de8e8cda5
taiseiiiii <isemiya.0509@gmail.com>
2026-01-08 19:58:22 -0800

    feat: impl contact page

---
+++ final-project/src/data/contact.ts
@@ -0,0 +1,52 @@
+export const FORMSPREE_ID = "maqnrnve";
+
+// Contact Usコンポーネントに渡すデータ
+export const contactData = {
+  // Hero Section
+  heroTitle: "Contact us",
+  heroSubtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+
+  email: {
+    address: "hello@Ddsgnr.io",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in ero.",
+  },
+
+  chat: {
+    linkText: "Start new chat",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in ero.",
+  },
+
+  phone: {
+    number: "+1 (555) 000-0000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in ero.",
+  },
+
+  office: {
+    address: "123 Sample St, Sydney NSW 2000 AU",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in ero.",
+  },
+
+  // Form Section
+  formTagline: "Tagline",
+  formTitle: "Contact us",
+  formDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  formImageUrl:
+    "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?w=800&q=80",
+  formImageAlt: "Mercedes-Benz car interior",
+
+  services: [
+    "Car Wash",
+    "Oil Change",
+    "Tire Service",
+    "Engine Repair",
+    "Body Work",
+    "Detailing",
+    "Other",
+  ],
+
+  termsUrl: "/terms",
+};
